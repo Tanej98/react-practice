d2f4d61d8ca9956c318e8fe6900d71109428dd6c
Ubuntu <tanej@linux-react.ko3r31e05dceleolvgwpleza5g.ux.internal.cloudapp.net>
2023-02-02 23:43:18 +0000

    completed styles in react

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
   const formSubmitHandler = event => {
     event.preventDefault();
-    props.onAddGoal(enteredValue);
+    if(enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
+    props.onAddGoal(enteredValue.trim());
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
+      {/* Dynamic class , for input element*/}
+       {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
+      <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
+       {/* Dynamic style */}
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
